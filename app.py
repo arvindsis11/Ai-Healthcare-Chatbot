2b3f5770debf724df49ef8d3a0d933e0ad6010f8
arvind sisodia <57572062+arvindsis11@users.noreply.github.com>
2023-06-16 02:40:30 +0530

    fix

--- app.py
+++ app.py
@@ -3,25 +3,28 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 import os
 
-app = Flask(__name__)
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
-filenumber = int(os.listdir('saved_conversations')[-1])
-filenumber += 1
-file = open('saved_conversations/' + str(filenumber), "w+")
+filenumber=int(os.listdir('saved_conversations')[-1])
+filenumber=filenumber+1
+file= open('saved_conversations/'+str(filenumber),"w+")
 file.write('bot : Hi There! I am a medical chatbot. You can begin conversation by typing in a message and pressing enter.\n')
 file.close()
 
-english_bot = ChatBot('Bot',
-                      storage_adapter='chatterbot.storage.SQLStorageAdapter',
-                      logic_adapters=[
-                          {
-                              'import_path': 'chatterbot.logic.BestMatch'
-                          }
-                      ],
-                      trainer='chatterbot.trainers.ListTrainer')
+app = Flask(__name__)
 
-english_bot.set_trainer(ChatterBotCorpusTrainer)
-english_bot.train('chatterbot.corpus.english')
+
+english_bot = ChatBot('Bot',
+             storage_adapter='chatterbot.storage.SQLStorageAdapter',
+             logic_adapters=[
+   {
+       'import_path': 'chatterbot.logic.BestMatch'
+   },
+   
+],
+trainer='chatterbot.trainers.ListTrainer')
+english_bot.set_trainer(ListTrainer)
 
 @app.route("/")
 def home():
@@ -32,13 +35,14 @@ def get_bot_response():
     userText = request.args.get('msg')
     response = str(english_bot.get_response(userText))
 
-    appendfile = os.listdir('saved_conversations')[-1]
-    appendfile = open('saved_conversations/' + str(filenumber), "a")
-    appendfile.write('user : ' + userText + '\n')
-    appendfile.write('bot : ' + response + '\n')
+    appendfile=os.listdir('saved_conversations')[-1]
+    appendfile= open('saved_conversations/'+str(filenumber),"a")
+    appendfile.write('user : '+userText+'\n')
+    appendfile.write('bot : '+response+'\n')
     appendfile.close()
 
     return response
+
 
 if __name__ == "__main__":
     app.run()
--- train.py
+++ train.py
@@ -1,19 +1,18 @@
-from chatterbot import ChatBot
+from chatterbot import ChatBot, utils
 from chatterbot.trainers import ListTrainer
 import os
 
 try:
-	os.remove("db.sqlite3")
-	print("Old database removed. Training new database")
-except:
-	print('No database found. Creating new database.')
+    os.remove("db.sqlite3")
+    print("Old database removed. Training new database")
+except FileNotFoundError:
+    print('No database found. Creating new database.')
 
 english_bot = ChatBot('Bot')
 english_bot.set_trainer(ListTrainer)
-for file in os.listdir('data'):
-        print('Training using '+file)
-        convData = open('data/' + file).readlines()
-        english_bot.train(convData)
-        print("Training completed for "+file)
-    
 
+for file in os.listdir('data'):
+    print('Training using ' + file)
+    convData = open('data/' + file, 'r').readlines()
+    english_bot.train(convData)
+    print("Training completed for " + file)
